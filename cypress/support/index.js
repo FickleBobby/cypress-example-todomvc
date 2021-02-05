d48784e034d396dfc563d1940562819b43c0d9c2
Steven Howard <steven.howard@hee.nhs.uk>
2021-02-05 10:36:53 +0000

    update e2e

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -31,19 +31,3 @@ Cypress.on('test:after:run', (test, runnable) => {
 
 	addContext({ test }, videoUrl);
 });
-
-beforeEach(() => {
-	cy.visit('./');
-	if (cy.get('button#rcc-confirm-button')) {
-		cy.get('button#rcc-confirm-button').click();
-	}
-	if (cy.get('input[name=username]')) {
-		cy.get('input[name=username]').type(Cypress.env('username'));
-		cy.get('input[name=password]').type(
-			`${Cypress.env('password')}{enter}`,
-			{
-				log: false,
-			}
-		);
-	}
-});
