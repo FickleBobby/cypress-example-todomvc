f2349a5241d31a2d51e84fa10f719e15907f8297
FickleBobby <64523325+FickleBobby@users.noreply.github.com>
2021-03-04 21:30:41 +0000

    Update app_spec.js

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -57,7 +57,7 @@ describe('TodoMVC - React', function () {
 	it('adds 2 todos', function () {
 		cy.get('.new-todo').type('learn testing{enter}').type('be cool{enter}');
 
-		cy.get('.todo-list li').should('have.length', 112);
-		cy.get('.todo-list li').should('have.length', 12);
+		cy.get('.todo-list li').should('have.length', 2);
+
 	});
 });
