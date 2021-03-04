64f6b9e46eba14cd38e9956997e35339fa406f3a
Steven Howard <steven.howard@hee.nhs.uk>
2021-03-04 20:16:13 +0000

    e2e:update tests

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -57,6 +57,6 @@ describe('TodoMVC - React', function () {
 	it('adds 2 todos', function () {
 		cy.get('.new-todo').type('learn testing{enter}').type('be cool{enter}');
 
-		cy.get('.todo-list li').should('have.length', 12);
+		cy.get('.todo-list li').should('have.length', 112);
 	});
 });
